6c6dcaede37cebc275324479d9b146b153fd4521
Dilya Joseph <dilyajoseph@gmail.com>
2021-02-02 16:43:58 -0600

    entire portfolio

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -4,7 +4,7 @@ import { SocialIcon } from 'react-social-icons';
 
 export default function NavBar() {
     return (
-        <header className="bg-red-800">
+        <header className="bg-purple-900">
             <div className="container mx-auto flex justify-between">
                 <nav className="flex">
                     <NavLink to="/" 
@@ -13,23 +13,24 @@ export default function NavBar() {
                     className="inline-flex items-center py-6 px-3 mr-4 text-red-100 hover:text-yellow-300 text-4xl font-bold cursive tracking-widest">
                         Dilya
                     </NavLink>
-                    <NavLink to="/post"
-                    className="inline-flex items-center py-3 px-3 my-6 rounded text-red-200 hover:text-yellow-400"
+                    <NavLink to="/about"
+                    className="inline-flex items-center py-3 px-3 my-8 rounded text-red-200 hover:text-yellow-400"
                     activeClassName="text-red-100 bg-red-700" 
                     >
-                        Blog Posts
+                        About Me
                     </NavLink>
                     <NavLink to="/project"
-                    className="inline-flex items-center py-3 px-3 my-6 rounded text-red-200 hover:text-yellow-400"
+                    className="inline-flex items-center py-3 px-3 my-8 rounded text-red-200 hover:text-yellow-400"
                     activeClassName="text-red-100 bg-red-700" 
                     >
                         Projects
                     </NavLink>
-                    <NavLink to="/about"
-                    className="inline-flex items-center py-3 px-3 my-6 rounded text-red-200 hover:text-yellow-400"
+                
+                    <NavLink to="/post"
+                    className="inline-flex items-center py-3 px-3 my-8 rounded text-red-200 hover:text-yellow-400"
                     activeClassName="text-red-100 bg-red-700" 
                     >
-                        About Me!
+                        Blog Posts
                     </NavLink>
                 </nav>
                 <div className="inline-flex py-3 px-3 my-6">
--- src/components/Post.js
+++ src/components/Post.js
@@ -25,7 +25,7 @@ export default function Post() {
     return (
         <main className="bg-yellow-900 min-h-screen p-12">
             <section className="container mx-auto">
-                <h1 className="text-5xl flex justify-center cursive">Dilya Joseph-Baz</h1>
+                <h1 className="text-5xl text-yellow-600 flex justify-center cursive">Dilya Joseph-Baz</h1>
                 <h2 className="text-lg text-yellow-100 flex justify-center mb-12">A lifestyle blog.</h2>
                 <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-8">
                     {postData && postData.map((post, index) => (
